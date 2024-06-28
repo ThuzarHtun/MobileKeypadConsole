d53e07908722472220768c48c2d48ee17f8742cf
ThuzarHtun <thuzar.nut@gmail.com>
2024-06-28 16:37:31 +0700

    Initial Version

---
+++ MobileKeypadConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using MobileKeypadConsole;
+using Timer = System.Timers.Timer;
+
+class Program
+{
+    static void Main()
+    {
+        var oldPhoneKeypad = new OldPhoneKeypad();
+        
+        Console.WriteLine("Enter the sequence of numbers (e.g., 2 for A, 22 for B, etc.):");
+
+        while (true)
+        {
+            string key = Console.ReadKey(true).KeyChar.ToString();
+            var result = OldPhonePad(key);
+            if (!string.IsNullOrEmpty(result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+
+    public static string OldPhonePad(string input)
+    {
+        return OldPhoneKeypad.ProcessInput(input);
+    }
+}
